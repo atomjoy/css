64d5d173323e7e8fd05fac406b9fa71f14eef1a1
Atomjoy <atomjoy.official@gmail.com>
2025-02-25 12:28:28 +0100

    LoadSvg.vue
    How to import svg from Vue 3 component.

---
+++ LoadSvg.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="rawsvg" v-html="svg"></div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const props = defineProps({
+	name: {
+		type: String,
+		required: true,
+	},
+});
+
+let svg = ref('');
+
+onMounted(async () => {
+	// Only from puclic directory
+	const res = await fetch(`/svg/${props.name}.svg?raw`);
+	svg.value = await res.text();	
+});
+</script>
